c12f8095fe0e7f5d92cb7f4b1c76e2d81d64b02d
adrien caubel <adriencaubel@macbook-pro-de-adrien.home>
2026-07-05 13:40:44 +0200

    cors

--- backend/src/main/java/fr/adriencaubel/vrp/config/CorsConfig.java
+++ backend/src/main/java/fr/adriencaubel/vrp/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://backend:*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
