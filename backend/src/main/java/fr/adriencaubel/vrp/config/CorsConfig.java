f3462ae6c5f35478a68d57b564fd83b1b41b7260
adrien caubel <adriencaubel@macbook-pro-de-adrien.home>
2026-07-05 16:08:31 +0200

    cors

--- backend/src/main/java/fr/adriencaubel/vrp/config/CorsConfig.java
+++ backend/src/main/java/fr/adriencaubel/vrp/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://backend:*")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "http://carto.traino.cloud*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
